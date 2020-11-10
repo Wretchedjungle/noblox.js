ad8604ad7eddfa6bbf4f2dae83652521db7f12d9
suufi <12759332+suufi@users.noreply.github.com>
2020-11-10 02:12:47 +0000

    Use jest/recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,10 @@ module.exports = {
     commonjs: true,
     es6: true,
     node: true,
-    'jest/globals': true
   },
   extends: [
-    'standard'
+    'standard',
+    'plugin:jest/recommended'
   ],
   globals: {
     Atomics: 'readonly',
